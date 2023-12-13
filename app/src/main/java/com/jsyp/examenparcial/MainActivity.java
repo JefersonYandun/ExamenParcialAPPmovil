e8990cac7cbfc5dfecc46aeba85834f4dbdd2408
JefersonYandun <jeferson.yandun237@ist17dejulio.edu.ec>
2023-12-13 10:44:24 -0500

    Funcionalidad acitvity Main

--- app/src/main/java/com/jsyp/examenparcial/MainActivity.java
+++ app/src/main/java/com/jsyp/examenparcial/MainActivity.java
@@ -1,15 +1,15 @@
 package com.jsyp.examenparcial;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Declara los EditText
     private EditText txtCedula, txtNombreProp, txtPlaca, txtAnio, txtMarca, txtColor, txtTipoVehiculo, txtMultas, txtValor;
 
     @Override
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Inicializa los EditText
         txtCedula = findViewById(R.id.txtCedula);
         txtNombreProp = findViewById(R.id.txtNombreProp);
         txtPlaca = findViewById(R.id.txtPlaca);
@@ -28,19 +27,16 @@ public class MainActivity extends AppCompatActivity {
         txtMultas = findViewById(R.id.txtMultas);
         txtValor = findViewById(R.id.txtValor);
 
-        // Configura el evento clic del botón
         Button btnCalcular = findViewById(R.id.btnCalcular);
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Llama a la función para procesar los cálculos
                 procesarCalculos();
             }
         });
     }
 
     private void procesarCalculos() {
-        // Obtén datos de los EditText
         String cedula = txtCedula.getText().toString();
         String nombreProp = txtNombreProp.getText().toString();
         String placa = txtPlaca.getText().toString();
@@ -49,24 +45,76 @@ public class MainActivity extends AppCompatActivity {
         String color = txtColor.getText().toString();
         String tipoVehiculo = txtTipoVehiculo.getText().toString();
         String multas = txtMultas.getText().toString();
-
-        // Obtén el valor del vehículo ingresado por el usuario
         String valorVehiculoStr = txtValor.getText().toString();
 
-        // Verifica si el campo de valor del vehículo no está vacío
         if (valorVehiculoStr.isEmpty()) {
             Toast.makeText(this, "Por favor, ingrese el valor del vehículo", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        // Convierte el valor del vehículo a double
         double valorVehiculo = Double.parseDouble(valorVehiculoStr);
+        boolean tieneMultas = !multas.isEmpty();
 
-        // Resto del código...
-        // (Añade el resto del código de cálculos después de esta sección)
+        double importeRenovacion = calcularImporteRenovacion(cedula, placa);
+        double multaContaminacion = calcularMultaContaminacion(Integer.parseInt(anio));
+        double valorMatriculacion = calcularValorMatriculacion(marca, tipoVehiculo, valorVehiculo);
+        double multaPorMultas = calcularMultaPorMultas(tieneMultas);
+
+        double totalPagar = importeRenovacion + multaContaminacion + valorMatriculacion + multaPorMultas;
+
+        Intent intent = new Intent(MainActivity.this, activity_resultados_vehiculo.class);
+        intent.putExtra("NOMBRE_PROPIETARIO", nombreProp);
+        intent.putExtra("IMPORTE_RENOVACION", importeRenovacion);
+        intent.putExtra("MULTA_CONTAMINACION", multaContaminacion);
+        intent.putExtra("VALOR_MATRICULACION", valorMatriculacion);
+        intent.putExtra("MULTA_POR_MULTAS", multaPorMultas);
+        intent.putExtra("TOTAL_PAGAR", totalPagar);
+        startActivity(intent);
     }
 
+    private double calcularImporteRenovacion(String cedula, String placa) {
+        double sueldoBasico = 435.0;
 
-    // Resto del código...
+        if (cedula.startsWith("1") && placa.contains("I")) {
+            return sueldoBasico * 0.05;
+        }
+        return 0.0;
+    }
 
+    private double calcularMultaContaminacion(int anioFabricacion) {
+        if (anioFabricacion < 2010) {
+            double porcentajeContaminacion = (2010 - anioFabricacion) * 0.02;
+            return porcentajeContaminacion;
+        }
+        return 0.0;
+    }
+
+    private double calcularValorMatriculacion(String marca, String tipoVehiculo, double valorVehiculo) {
+        double porcentajeMatriculacion = 0.0;
+
+        if (marca.equalsIgnoreCase("Toyota")) {
+            if (tipoVehiculo.equalsIgnoreCase("Jeep")) {
+                porcentajeMatriculacion = 0.08;
+            } else if (tipoVehiculo.equalsIgnoreCase("Camioneta")) {
+                porcentajeMatriculacion = 0.12;
+            }
+        } else if (marca.equalsIgnoreCase("Suzuki")) {
+            if (tipoVehiculo.equalsIgnoreCase("Vitara")) {
+                porcentajeMatriculacion = 0.10;
+            } else if (tipoVehiculo.equalsIgnoreCase("Automóvil")) {
+                porcentajeMatriculacion = 0.09;
+            }
+        }
+
+        return valorVehiculo * porcentajeMatriculacion;
+    }
+
+    private double calcularMultaPorMultas(boolean tieneMultas) {
+        double sueldoBasico = 435.0;
+
+        if (tieneMultas) {
+            return sueldoBasico * 0.25;
+        }
+        return 0.0;
+    }
 }
--- app/src/main/java/com/jsyp/examenparcial/activity_resultados_vehiculo.java
+++ app/src/main/java/com/jsyp/examenparcial/activity_resultados_vehiculo.java
@@ -1,8 +1,9 @@
 package com.jsyp.examenparcial;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class activity_resultados_vehiculo extends AppCompatActivity {
 
@@ -10,5 +11,19 @@ public class activity_resultados_vehiculo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultados_vehiculo);
+
+        Intent intent = getIntent();
+        String nombrePropietario = intent.getStringExtra("NOMBRE_PROPIETARIO");
+        double importeRenovacion = intent.getDoubleExtra("IMPORTE_RENOVACION", 0.0);
+
+        // Usa los datos como sea necesario y actualiza los TextViews
+        TextView lblNombreResult = findViewById(R.id.lblNombreResult);
+        lblNombreResult.setText(nombrePropietario);
+
+        TextView lblRenovacionPlacas = findViewById(R.id.lblRenovacionPlacas);
+        lblRenovacionPlacas.setText(String.valueOf(importeRenovacion));
+
+        // Actualiza otros TextViews según sea necesario
+        // ...
     }
 }
